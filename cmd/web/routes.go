866116932f9cd94c05a1a44bf81bc5acf35f4af5
Uday Kiran N <56758575+udaycruise2903@users.noreply.github.com>
2022-12-26 15:16:51 +0530

    added pkg

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,7 +3,6 @@ package main
 import "net/http"
 
 func (app *application) routes() *http.ServeMux {
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/card", app.showCard)
---
+++ pkg/models/mysql/cards.go
@@ -0,0 +1,24 @@
+package mysql
+
+import (
+	"database/sql"
+)
+
+type CardModel struct {
+	DB *sql.DB
+}
+
+// To insert a new card into the database
+func (m *CardModel) Insert(title, content, expires string) (int, error) {
+	return 0, nil
+}
+
+// to return a specific card based on its id
+func (m *CardModel) Get(id int) (*models.Card, error) {
+	return nil, nil
+}
+
+// to return the 10 most recently created cards
+func (m *CardModel) Latest() ([]*models.Card, error) {
+	return nil, nil
+}
